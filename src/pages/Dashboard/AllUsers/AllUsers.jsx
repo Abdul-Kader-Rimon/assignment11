12f7bcdf11750a246066d20a308ee2e1e8b318b6
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-12-17 11:34:58 +0600

    Admin make volunteer and donor section

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -5,9 +5,10 @@ import { AuthContext } from '../../../Context/AuthContext';
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure()
     const [users, setUsers] = useState([]);
+    const [filter , setFilter] = useState('')
  
     const fetchUsers = () => {
-           axiosSecure.get('/users')
+           axiosSecure.get(`/users${filter ? `?status=${filter}`: ""}`)
             .then(res => {
             setUsers(res.data)
         })
@@ -17,7 +18,7 @@ const AllUsers = () => {
     useEffect(() => {
       fetchUsers()
      
-    },[axiosSecure ])
+    },[ filter ])
 
     const handleStatusChange = (email, status) => {
         axiosSecure.patch(`/update/user/status?email=${email}&status=${status}`)
@@ -25,27 +26,42 @@ const AllUsers = () => {
             console.log(res.data)
             fetchUsers();
         })
-
-
+    } 
+    const handleRoleChange = (email, role) => {
+        axiosSecure.patch(`/users/make-${role}/${email}`)
+            .then( () => {
+            
+            fetchUsers();
+        })
     } 
  
 
     return (
       <div className="overflow-x-auto">
+        <h2 className="text-xl font-bold mb-4">All Users</h2>
+
+        <select
+          className="select select-bordered mb-4"
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="">All Users</option>
+          <option value="active">Active</option>
+          <option value="blocked">Blocked</option>
+        </select>
+
         <table className="table">
           <thead>
             <tr>
-              
-              <th>Name</th>
+              <th>User</th>
               <th>Role</th>
-              <th>User Status</th>
+              <th>Status</th>
+              <th>Action</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {users?.map((user) => (
-              <tr>
-                 
+              <tr key={user?._id}>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -63,7 +79,16 @@ const AllUsers = () => {
                   </div>
                 </td>
                 <td>{user?.role}</td>
-                <td>{user?.status}</td>
+
+                <td>
+                  <span
+                    className={`badge ${
+                      user.status === "active" ? "badge-success" : "badge-error"
+                    }`}
+                  >
+                    {user?.status}
+                  </span>
+                </td>
                 <th>
                   {user?.status == "active" ? (
                     <button
@@ -78,6 +103,22 @@ const AllUsers = () => {
                       className="btn btn-ghost btn-xs"
                     >
                       Active
+                    </button>
+                  )}
+
+                  {user?.role === "Donor" ? (
+                    <button
+                      onClick={() => handleRoleChange(user?.email, "volunteer")}
+                      className="btn btn-primary btn-xs ml-5"
+                    >
+                      Volunteer
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleRoleChange(user?.email, "Donor")}
+                      className="btn btn-primary btn-xs ml-5"
+                    >
+                       Donor
                     </button>
                   )}
                 </th>
