1226e7fdc0d3658ab8094f63ca69ea45013b2d95
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-12-20 15:27:56 +0600

    DasdboardLayOut rsposive

--- src/DashboardLayOut/DashboardLayOut.jsx
+++ src/DashboardLayOut/DashboardLayOut.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Aside from '../components/Aside/Aside';
+import { useState } from "react";
+import { Outlet } from "react-router";
+import Aside from "../components/Aside/Aside";
+import { Menu, X } from "lucide-react";
 
 const DashboardLayOut = () => {
-    return (
-        <div className='flex'>
-            
-            <Aside  />
-            <div className='flex-1 p-5'>
-                <Outlet/>
-            </div>
-            
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="flex min-h-screen relative">
+      {open && (
+        <div
+          onClick={() => setOpen(false)}
+          className="fixed inset-0 bg-black/40 z-40 md:hidden"
+        />
+      )}
+      <div
+        className={`fixed md:static z-50 transition-transform duration-300
+        ${open ? "translate-x-0" : "-translate-x-full"}
+        md:translate-x-0`}
+      >
+        <Aside />
+      </div>
+      <div className="flex-1 p-5">
+        <div className="md:hidden mb-4">
+          <button
+            onClick={() => setOpen(!open)}
+            className="btn btn-sm bg-[#422ad5] text-white"
+          >
+            {open ? <X size={20} /> : <Menu size={20} />}
+          </button>
         </div>
-    );
+
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayOut;
